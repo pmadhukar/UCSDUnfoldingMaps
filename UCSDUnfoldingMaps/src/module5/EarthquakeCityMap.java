ea82c0d9f91bc14a71d06d9e24cabe5b6f0222c9
Pratyushotpal Madhukar <pratyush.madhukar@gmail.com>
2016-02-09 11:57:38 +0530

    Update EarthquakeCityMap.java

--- UCSDUnfoldingMaps/src/module5/EarthquakeCityMap.java
+++ UCSDUnfoldingMaps/src/module5/EarthquakeCityMap.java
@@ -20,8 +20,7 @@ import processing.core.PApplet;
 /** EarthquakeCityMap
  * An application with an interactive map displaying earthquake data.
  * Author: UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
- * Date: July 17, 2015
+ * @author Pratyushotpal Madhukar
  * */
 public class EarthquakeCityMap extends PApplet {
 
