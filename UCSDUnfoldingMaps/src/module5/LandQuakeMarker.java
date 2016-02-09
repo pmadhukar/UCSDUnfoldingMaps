966945a236fd776321fdf925ae5f81dad717d906
Pratyushotpal Madhukar <pratyush.madhukar@gmail.com>
2016-02-09 12:04:34 +0530

    Update LandQuakeMarker.java

--- UCSDUnfoldingMaps/src/module5/LandQuakeMarker.java
+++ UCSDUnfoldingMaps/src/module5/LandQuakeMarker.java
@@ -6,7 +6,7 @@ import processing.core.PGraphics;
 /** Implements a visual marker for land earthquakes on an earthquake map
  * 
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * @author Pratyushotpal Madhukar
  *
  */
 public class LandQuakeMarker extends EarthquakeMarker {
